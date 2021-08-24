40484da51d0cdc1d94b11d51509697cc5a4d96ad
Derzz <michaelx.10@hotmail.com>
2021-08-23 21:48:57 -0400

    Minor Edits to Game.java

--- src/pkg2048gui/Menu.java
+++ src/pkg2048gui/Menu.java
@@ -6,9 +6,6 @@
  */
 package pkg2048gui;
 
-//Imports
-import java.awt.Dimension;
-import javax.swing.JFrame;
 
 class Menu extends javax.swing.JFrame {
 
@@ -184,15 +181,12 @@ class Menu extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        
         //</editor-fold>
         //</editor-fold>
 
